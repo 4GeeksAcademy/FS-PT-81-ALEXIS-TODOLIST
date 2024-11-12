b9e6e7289d504eefbebb5cd57ca56250f8c3b8c6
Cadivals <4geeksalex@gmail.com>
2024-11-12 00:03:42 +0000

    feast:todoList

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,59 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-//include images into your bundle
-import rigoImage from "../../img/rigo-baby.jpg";
+import ReactDOM from 'react-dom/client'
 
-//create your first component
 const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
-	);
+	const [task, setTask] = useState("");
+	const [taskList, setTaskList] = useState ([]);
+	const [taskCount, setTaskCount] = useState( );
+
+  
+  useEffect(() => {
+    setTaskCount(taskList.length);
+  }, [taskList]); 
+
+	const handleSubmit = e => {
+		e.preventDefault();
+	
+		setTaskList([...taskList, task]);
+		setTask('');
+	}
+	const handleChange = e =>{
+		setTask (e.target.value)
+	}
+
+	const handleDelete = (index) =>{
+		setTaskList(taskList.filter((_, i)=>i!= index));
+	};
+		return (
+			<div className="text-center">
+				<h1 className="text-center mt-5">todos</h1>
+				<form onSubmit={handleSubmit}>
+				<input type="text" value={task} onChange={handleChange} 
+				placeholder="Add a new task" />
+				<button>Add task</button>
+				</form>
+
+				<ul>
+				{taskList.length > 0 
+           ? taskList.map((task, i) => 
+               <li key={i}>
+        {task}
+        <i className="fa-solid fa-xmark" onClick={() => handleDelete(i)} /> 
+      </li>) 
+  : <li>Add task</li>}
+<div className="task-count">
+        {taskCount === 1 ? `${taskCount} item task added` : `${taskCount} items task added`}
+      </div>
+				</ul>
+				
+			</div>
+		);
 };
 
+
+
 export default Home;
+const root = ReactDOM.createRoot(document.querySelector('#app'));
+root.render(<Home />)
+
